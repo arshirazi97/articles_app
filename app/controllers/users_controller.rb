d5b446337172e2c5d0169e2a1c6db7d2948a3df0
arshirazi97 <90085179+arshirazi97@users.noreply.github.com>
2021-09-19 15:50:36 +0500

    Make appproduction ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,15 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
+    def show
+        @user = User.find(params[:id]) 
+        @articles = @user.articles
+    end
+    
+    def index
+        @users = User.all
+    end
+
     def edit
         @user = User.find(params[:id])
     end
@@ -11,7 +20,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your account information was successfully updated"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
